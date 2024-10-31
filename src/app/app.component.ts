16a1997288d9b6a60570688767295a1796211d07
Josh Cemine <j.jcemine@gmail.com>
2024-10-31 11:28:44 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxButtonModule, DxCheckBoxComponent, DxCheckBoxModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxCheckBoxModule, DxButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'repeatedFire';
+
+  detail = { isRead: false }; // Example state to bind to the checkbox
+  @ViewChild('checkBox', { static: false }) checkBox!: DxCheckBoxComponent
+
+
+  onValueChangedRunning = false
+  onValueChanged(e: any): void {
+    if (this.onValueChangedRunning) return
+    this.onValueChangedRunning = true
+
+    if (e.event && e.event.type === 'dxclick') {
+      console.log("User Change")
+      this.changeValueByUser()
+    }
+    else {
+      console.log("Changed from other function")
+      this.changeValueByCode()
+    }
+
+    this.onValueChangedRunning = false
+  }
+
+
+  changeValueByUser() {
+    this.checkBox.instance.option('value', true)
+  }
+  changeValueByCode() {
+    this.checkBox.instance.option('value', false)
+  }
+
+
+  onClick(e: any) {
+    this.changeValueByCode()
+  }
+
+
 }
